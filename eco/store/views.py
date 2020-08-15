94baf2863783e854b64ad45ce913e17eb7b46d8f
Saness <sanishsuwal3@gmail.com>
2020-08-15 13:53:50 +0545

    All done
    The pusg conatins update for javascript integration and payment

--- eco/store/views.py
+++ eco/store/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from .models import *
+from django.http import JsonResponse
+import json
+import datetime
 
 # Create your views here.
 def store(request):
-    products = Product.objects.all()
-    context={'products':products}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete  =False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        items = []
+        order = {'get_car_total':0, 'get_cart_items':0, 'shipping':False}
+        cartItems = order['get_cart_items']
+
+    products = Product.objects.all() 
+    context={'products':products, 'cartItems':cartItems}
     return render(request, 'store/store.html', context)
 
 def cart(request):
@@ -12,10 +25,14 @@ def cart(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete  =False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+
     else:
         items = []
-        order = {'get_car_total':0, 'get_cart_items':0}
-    context={'items':items, 'order':order}
+        order = {'get_car_total':0, 'get_cart_items':0, 'shipping':False}
+        cartItems = order['get_cart_items']
+        
+    context={'items':items, 'order':order, 'cartItems':cartItems}
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
@@ -23,8 +40,64 @@ def checkout(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete  =False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         items = []
-        order = {'get_car_total':0, 'get_cart_items':0}
-    context={'items':items, 'order':order}
+        order = {'get_car_total':0, 'get_cart_items':0, 'shipping':False}
+        cartItems = order['get_cart_items']
+
+    context={'items':items, 'order':order, 'cartItems':cartItems}
     return render(request, 'store/checkout.html', context)
+
+def updateItem(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete  =False)
+
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+    if action =='add':
+        orderItem.quantity = (orderItem.quantity + 1)
+    elif action == 'remove':
+        orderItem.quantity = (orderItem.quantity - 1)
+    
+    orderItem.save()
+
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+    
+
+    return JsonResponse('Item was added', safe=False)
+
+def processOrder(request):
+    transaction_id = datetime.datetime.now().timestamp
+    data = json.loads(request.body)
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete  =False)
+        total = float(data['form']['total'])
+        order.transaction_id = transaction_id
+
+        if total == float(order.get_cart_total):
+            order.complete = True
+        
+        order.save()
+
+        if order.shipping ==True:
+            ShippingAddress.objects.create(
+                customer=customer,
+                order=order,
+                address = data['shipping']['address'],
+                city = data['shipping']['city'],
+                state = data['shipping']['state'],
+                zipcode = data['shipping']['zipcode'],
+            )
+    else:
+        print('User not logged in')
+    return JsonResponse('Payment complete', safe=False)
+
